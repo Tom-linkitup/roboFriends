648988fb036866c88b82212ad6c4535ae92b9f83
Tom <tom@TomdeMacBook-Air.local>
2020-06-13 16:40:42 +0800

    Updates

---
+++ precache-manifest.6a4d908f1b8306a18f6b2050116b2490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d693c65fda7a5307e8da0202a4b60b22",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "57ffa4fec271a853eb5f",
+    "url": "/robofriends/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3206089bab05f8a0aff",
+    "url": "/robofriends/static/js/2.1317703d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.1317703d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ffa4fec271a853eb5f",
+    "url": "/robofriends/static/js/main.f69c65cd.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
