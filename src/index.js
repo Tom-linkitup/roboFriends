463d99f3cf95c7ff6fce1c6f81ec39c1700f8ea8
Tom <tom@TomdeMacBook-Air.local>
2020-06-13 16:45:28 +0800

    change the title

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <h1>Hello Eva</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
